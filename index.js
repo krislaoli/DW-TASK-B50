7828faec76f190650666c70e55c6ad41d7e25bb6
Mei Kristian Laoli <laolikristian1305@gmail.com>
2023-10-14 21:12:30 +0700

    Commit 2

--- index.js
+++ index.js
@@ -1,17 +1,48 @@
-const express = require ('express')
-const path = require('path')
-const app = express()
-const port = 5000
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 5000;
 
-// app.set("view engine", "hbs");
-// app.set("views", path.join(__dirname, "src/views"));
+// pemanggilan untuk views hbs ditampilakn ke web
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "src/views"));
 
-app.get('/', ( req, res) => {
-    res.send("apa kabar kamu disana")
-})
+// routing
+app.get("/blog-detail", blogDetail);
+app.get("/blog", blog);
+app.get("/contact", contact);
+app.get("/", home);
+app.get("/testimonial", testimonial);
 
-
+// set static file server
+app.use(express.static('src/assets'))
 
 app.listen(port, () => {
-    console.log("app")
-})
+  console.log("app");
+});
+
+function blogDetail(req, res) {
+  res.render("blog-detail");
+}
+
+function blog(req, res) {
+  res.render("blog");
+}
+
+function contact(req, res) {
+  res.render("contact");
+}
+
+function home(req, res) {
+  res.render("index");
+}
+
+function testimonial(req, res) {
+  res.render("testimonial");
+}
+
+
+
+
+
+

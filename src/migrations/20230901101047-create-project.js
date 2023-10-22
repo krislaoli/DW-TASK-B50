79edbf3fd8b0e1a5347e01369553a3b5c22eefc1
Mei Kristian Laoli <laolikristian1305@gmail.com>
2023-10-22 22:23:28 +0700

    Commit 1

---
+++ src/migrations/20230901101047-create-project.js
@@ -0,0 +1,60 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Projects', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      author: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'users',
+          key: 'id'
+        }
+      },
+
+      title: {
+        type: Sequelize.STRING
+      },
+      content: {
+        type: Sequelize.STRING
+      },
+      images: {
+        type: Sequelize.STRING
+      },
+      starDate: {
+        type: Sequelize.DATE
+      },
+      endDate: {
+        type: Sequelize.DATE
+      },
+      nodejs: {
+        type: Sequelize.BOOLEAN
+      },
+      reactjs: {
+        type: Sequelize.BOOLEAN
+      },
+      js: {
+        type: Sequelize.BOOLEAN
+      },
+      vuejs: {
+        type: Sequelize.BOOLEAN
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Projects');
+  }
+};
